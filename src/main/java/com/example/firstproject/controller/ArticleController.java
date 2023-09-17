1704ae3e9f538cf0f9fb74d2d4be92297dd0379c
psb <a01051767021@gmail.com>
2023-09-17 21:27:15 +0900

    MVC practice

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,14 +1,21 @@
 package com.example.firstproject.controller;
 
+
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Optional;
+
 @Controller
+@Slf4j // print문으로 확인하는 것이 서버 로그로 정보를 확인하는 어노테이션
 public class ArticleController {
     @Autowired //자동 주입
     private ArticleRepository articleRepository;
@@ -21,16 +28,31 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+      //  System.out.println(form.toString());
+        log.info(form.toString());
+
         Article article = form.toEntity(); // DTO를 Entity로 변환
-        System.out.println(article.toString());
 
+        //System.out.println(article.toString());
+        log.info(article.toString());
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
-
+        //System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 
+    @GetMapping("/articles/{id}")  // @PathVariable은 url 요청으로 온 전달값의 컨트롤러의 매개변수로 가져오는 어노테이션이다.
+    public String show(@PathVariable Long id, Model model) { // 매개변수로 id 받아 오기
+        log.info("id = " + id); // id를 잘 받았는지 확인하는 로그 찍기
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
 
 
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,35 +1,24 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity // entity -> DTO(data transfer object, 컨트롤러에 담기는 변수를 받아주는 역할)를 DB테이블로 바꿔주는 역할임, DB는 java언어를 모르기 때문에.
-
+@AllArgsConstructor
+@NoArgsConstructor // 매개변수 없는 기본 생성자임.. 상황에 따라서 써도 되고 안써도 됨.
+@ToString
 public class Article {
     @Id //key값, 데이터 구분 역할
-    @GeneratedValue // 대푯값 번호 자동 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 대푯값 번호 자동 지정
     private Long id;
     @Column //테이블의 두 필드를 DB에 연결해주는 역할
     private  String title;
     @Column //테이블의 두 필드를 DB에 연결해주는 역할
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
 }
